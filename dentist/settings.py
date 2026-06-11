2d25ab79e142e567aa4d07b524c3f384cc86643c
Demyan <159693578+dem4ik231213@users.noreply.github.com>
2026-06-11 19:42:14 +0300

    Update settings.py

--- dentist/settings.py
+++ dentist/settings.py
@@ -147,6 +147,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = 'dento.noreply@gmail.com'  #  Gmail
 EMAIL_HOST_PASSWORD = 'cxgq hgbb hzlf ftvh'  # App Password
 DEFAULT_FROM_EMAIL = 'Dento Clinic <dento.noreply@gmail.com>'  # ← теж свій email
